e372675c7f4ac2975c173c8bfb9a06431d475303
Matheus Plessmann <maplessmann@gmail.com>
2019-09-13 23:59:08 -0300

    feat: add babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    ]
+  }
+}
