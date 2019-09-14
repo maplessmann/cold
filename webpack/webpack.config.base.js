47c5d1d44afd4c638d1f9e7c735379fa02c45d98
Matheus Plessmann <maplessmann@gmail.com>
2019-09-14 10:19:33 -0300

    refactor: change source path name

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './app/index.js',
   output: {
     path: path.join(__dirname, '../dist'),
     filename: 'app.bundle.js'
