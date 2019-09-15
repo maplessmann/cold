849141c2d6e042dc1b8a3b26e18a265445919b35
Matheus Plessmann <maplessmann@gmail.com>
2019-09-15 15:55:38 -0300

    feat: use lazy and suspense to dynamic load components

---
+++ app/components/DynamicLoad/index.js
@@ -0,0 +1,11 @@
+import React, { Suspense } from 'react'
+
+const DynamicLoad = ({ children, when }) => {
+  return when && (
+    <Suspense fallback={null}>
+      {children}
+    </Suspense>
+  )
+}
+
+export default DynamicLoad
--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -24,7 +24,10 @@ module.exports = {
             ],
             '@babel/preset-react',
           ],
-          plugins: ['react-hot-loader/babel'],
+          plugins: [
+            'react-hot-loader/babel',
+            '@babel/plugin-syntax-dynamic-import',
+          ],
         },
       },
       {
