bd0123f67120f7c4d482bf88fc7ff7912b5609ee
Matheus Plessmann <maplessmann@gmail.com>
2019-09-15 19:35:19 -0300

    chore: clean up the app

--- app/App.js
+++ app/App.js
@@ -1,21 +1,9 @@
-import { useState, lazy } from 'react'
 import { hot } from 'react-hot-loader'
-import Button from 'components/Button'
-import DynamicLoad from 'components/DynamicLoad'
-const Message = lazy(() => import(`components/Message`))
 
 const App = () => {
-  const [isButtonClicked, setIsButtonClicked] = useState(false)
-
   return (
     <div className="app-layout">
-      <h1>Coold is cool!</h1>
-      <Button onClick={() => setIsButtonClicked(true)}>
-        Click to Asynchronously load a component
-      </Button>
-      <DynamicLoad when={isButtonClicked}>
-        <Message text="I was loaded with code-splitting!" />
-      </DynamicLoad>
+      <h1>❄️ Coold boilerplate!</h1>
     </div>
   )
 }
--- app/components/Message/index.js
+++
@@ -1,7 +0,0 @@
-import './Message.css'
-
-const Message = ({ text }) => {
-  return <strong className="message">{text}</strong>
-}
-
-export default Message
