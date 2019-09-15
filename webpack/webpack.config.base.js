f05da1e67972671e3010ec3b56632dd52a659029
Matheus Plessmann <maplessmann@gmail.com>
2019-09-14 22:56:00 -0300

    chore: add a dev build with hot loader

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -15,6 +15,7 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['react-hot-loader/babel']
         },
       },
       {
