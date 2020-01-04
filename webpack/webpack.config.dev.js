fa92f7c6a5f1ceed80d5bbe638599950c08c04e8
Matheus Plessmann <maplessmann@gmail.com>
2020-01-04 10:53:17 -0300

    chore: add host to devServer config

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -4,6 +4,7 @@ const baseConfig = require('./webpack.config.base')
 module.exports = merge(baseConfig, {
   mode: 'development',
   devServer: {
+    host: '0.0.0.0',
     port: 9000,
     historyApiFallback: true,
   },
