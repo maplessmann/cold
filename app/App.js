1bcc2514f60e4600c815b2580c6763bef79ca5a5
Matheus Plessmann <maplessmann@gmail.com>
2020-01-05 16:43:25 -0300

    chore: add normalize.css rules

--- app/App.js
+++ app/App.js
@@ -2,6 +2,8 @@ import { hot } from 'react-hot-loader/root'
 import Header from 'components/Header'
 import Routes from './routes'
 
+import 'styles/main.scss'
+
 const App = () => {
   return (
     <div className="app-layout">
